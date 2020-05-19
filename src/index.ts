abde6e8635e48665853c2ce606fc7bbe43e03034
Benoit Genevaux <genevauxb@hotmail.fr>
2020-05-19 13:49:53 +0200

    little config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import {RestClientService} from './client/rest-client-service';
+import {RestClient} from './client/rest-client.decorator';
 import {Body, Header, PathParam, QueryParam} from './client/parameters-decorator';
 import {DELETE, GET, POST, PUT} from './client/request-decorator';
 
 export default {
-    RestClientService,
+    RestClientService, RestClient,
     Body, PathParam, QueryParam, Header,
     GET, POST, PUT, DELETE
 };
